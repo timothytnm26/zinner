39a20c3bdfe304a11d46f185bebc1125c2415589
timothytnm <timothytnm@gmail.com>
2024-08-29 20:53:58 +0700

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,31 @@ import  { useRef } from 'react';
 import jsQR from 'jsqr';
 
 import { Scanner } from '@yudiel/react-qr-scanner'
-function getRandomColor() {
-    // Generate a random color in hexadecimal format
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+import useGoogleSheets from 'use-google-sheets';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+
+// Config variables
+const SPREADSHEET_ID = import.meta.env.VITE_SPREADSHEET_ID;
+const SHEET_ID = import.meta.env.VITE_SHEET_ID
+const CLIENT_EMAIL =  import.meta.env.VITE_GOOGLE_SERVICE_ACCOUNT_EMAIL
+const PRIVATE_KEY = import.meta.env.VITE_GOOGLE_PRIVATE_KEY.replace(/\n/g, '\n')
+const API_KEY = import.meta.env.API_KEY
+
+console.log({SPREADSHEET_ID})
+
 function App() {
   const [qrCode, setQrCode] = useState('')
   const [color, setColor] = useState('')
-
+  const { data, loading, error } = useGoogleSheets({
+    apiKey: "AIzaSyDDV7gs37iGZsH0mKlPxx275LFq3kn8LYY",
+    sheetId: "1suYYhsoL7YWVSfnWQ5rgeVJkSJEbAB2So8VWIXWMo2c",
+    sheetsOptions: [{ id: 'isChecked' }],
+  });
    const fileInputRef = useRef(null);
-
+  if (data) {
+  console.log(data)
+}
 const handleImageUpload = (event) => {
     event.preventDefault();
     event.stopPropagation();
@@ -64,16 +74,33 @@ const handleImageUpload = (event) => {
             }
           }
         };
-
         image.src = imageData;
       };
-
       reader.readAsDataURL(file);
     }
   };
+  function getRandomColor() {
+    // Generate a random color in hexadecimal format
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
   return (
-    <div>
-      <Scanner onScan={result => {
+    <div style={{
+      width: '100%',
+      height:'100%',
+    }
+    }>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        
+      }}>
+        <Scanner onScan={result => {
         setQrCode(result[0].rawValue)
         setColor(getRandomColor())
       }}
@@ -81,27 +108,8 @@ const handleImageUpload = (event) => {
         allowMultiple
         scanDelay={1000}
       />
-      
-      
-         <h1 style={{color:color}}>code: {qrCode}</h1>
-          <div>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageUpload}
-        ref={fileInputRef}
-        style={{ display: 'none' }}
-      />
-      <button
-        className="img-input"
-        onClick={(e) => {
-          e.stopPropagation();
-          fileInputRef.current?.click();
-        }}
-      >
-        SCAN
-      </button>
-    </div>
+      <h1 style={{color: color, maxWidth:"500px"}}>{qrCode}</h1>
+      </div>
     </div>
   )
 }
